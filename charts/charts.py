c613cbf5a76a4123d9df49fac910a5a537657ed4
Marcos Rodriguez Morales <marcosrm182@gmail.com>
2023-09-22 11:05:07 +0200

    Solucion

--- charts/charts.py
+++ charts/charts.py
@@ -5,6 +5,6 @@ def generate_pie_chart():
     values = [200, 34, 120]
 
     fig, ax = plt.subplots()
-    ax.pie(values, labels)
+    ax.pie(values, labels=labels)
     plt.savefig('pie.png')
     plt.close()
